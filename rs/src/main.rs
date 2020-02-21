bea1618a3623c2e1f1917e6cee7ef070d4b7b7bc
Guillaume Leroi <guillaume@leroi.re>
2020-02-21 01:00:14 +0100

    rs/DayEntry: handle formatting and comments

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,10 +1,12 @@
 extern crate chrono;
+extern crate chrono_locale;
 
-use std::str::FromStr;
-use std::fs;
-use std::io::BufRead;
+use crate::chrono_locale::LocaleDate;
 use std::error;
 use std::fmt;
+use std::fs;
+use std::io::BufRead;
+use std::str::FromStr;
 
 #[derive(PartialEq, Debug)]
 struct DayEntry {
@@ -16,7 +18,7 @@ struct DayEntry {
 
 #[derive(Debug)]
 struct ParseDayEntryError {
-    cause: String
+    cause: String,
 }
 
 impl fmt::Display for ParseDayEntryError {
@@ -26,7 +28,7 @@ impl fmt::Display for ParseDayEntryError {
 }
 
 impl error::Error for ParseDayEntryError {
-    fn source(&self) ->  Option<&(dyn error::Error + 'static)> {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         Some(self)
     }
 }
@@ -35,19 +37,60 @@ impl FromStr for DayEntry {
     type Err = Box<dyn error::Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts : Vec<&str> = s.split_whitespace().collect();
+        let parts: Vec<&str> = s.split_whitespace().collect();
         if parts.len() != 5 {
-            return Err(Box::new(ParseDayEntryError{cause: "invalid fields count".to_owned()}));
+            return Err(Box::new(ParseDayEntryError {
+                cause: "invalid fields count".to_owned(),
+            }));
         }
-        Ok(DayEntry{
-            day: chrono::NaiveDate::parse_from_str(parts[1], "%d/%m/%Y").or_else(|err| {
-                Err(format!("invalid date: {}", err))
-            })?,
+        Ok(DayEntry {
+            day: chrono::NaiveDate::parse_from_str(parts[1], "%d/%m/%Y")
+                .or_else(|err| Err(format!("invalid date: {}", err)))?,
             hours: parts[2].parse::<f64>()?,
             fee: parts[3].parse::<f64>()?,
             meal: parts[4].parse::<f64>()?,
         })
     }
+}
+
+fn capitalize(s: &str) -> String {
+    let mut res = String::with_capacity(s.len());
+    for (i, c) in s.chars().enumerate() {
+        if i == 0 {
+            res.push_str(&c.to_uppercase().to_string());
+        } else {
+            res.push_str(&c.to_lowercase().to_string());
+        }
+    }
+    res
+}
+
+impl ToString for DayEntry {
+    fn to_string(&self) -> String {
+        let name = capitalize(&self.day.formatl("%a", "fr_FR").to_string());
+        return format!(
+            "{name} {day} {hours:.02} {fee:.02} {meal:.02}",
+            name = name,
+            day = self.day.format("%d/%m/%Y"),
+            hours = self.hours,
+            fee = self.fee,
+            meal = self.meal
+        );
+    }
+}
+
+fn read_sheet<I>(lines: I) -> Vec<DayEntry>
+where
+    I: IntoIterator<Item = String>,
+{
+    let mut entries = Vec::with_capacity(31);
+    for line in lines {
+        if line.starts_with('#') || line.is_empty() {
+            continue;
+        }
+        entries.push(line.parse::<DayEntry>().expect(&line));
+    }
+    entries
 }
 
 #[cfg(test)]
@@ -57,22 +100,25 @@ mod tests {
     #[test]
     fn parse_day_entry() {
         let input = "Mer 13/11/2019 4.50 4.10 3.00";
-        let expected = DayEntry{
+        let expected = DayEntry {
             day: chrono::NaiveDate::from_ymd(2019, 11, 13),
             hours: 4.5,
             fee: 4.1,
             meal: 3.0,
         };
-        let actual : DayEntry = input.parse().expect("could not parse day entry");
+        let actual: DayEntry = input.parse().expect("could not parse day entry");
         assert_eq!(actual, expected);
+
+        let output = actual.to_string();
+        assert_eq!(output, input);
     }
 }
 
 fn main() {
     let r = fs::File::open("2019_11_allison.txt").expect("could not open file");
     let buf = std::io::BufReader::new(r);
-
-    for line in buf.lines() {
-        println!("{}", line.expect("error reading line"));
+    let entries = read_sheet(buf.lines().map(|l| l.unwrap()));
+    for entry in entries {
+        println!("{}", entry.to_string());
     }
 }
